eae7986a740af3d274dbb5e5d6b25e7de2b8f7ae
rita <rita.merhej@hotmail.com>
2024-04-27 09:52:12 +0300

    setup/ install tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,11 @@
 import { Provider } from "react-redux";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import store from "./store/store";
-import Login from "./features/Login/Login";
-import ErrorPage from "./features/Error/Error";
-import AllNews from "./features/AllNews/AllNews";
-import Signup from "./features/Signup/Signup";
-import DailyNews from "./features/DailyNews/DailyNews";
-import PostDetails from "./features/PostDetails/PostDetails";
-import PublicRoute from "./components/templates/PublicRoute";
-import ProtectedRoute from "./components/templates/ProtectedRoute";
+import MainNavigator from "./navigation/MainNavigator";
 
 function App() {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: (
-        <PublicRoute>
-          <Login />
-        </PublicRoute>
-      ),
-      errorElement: <ErrorPage />,
-    },
-    {
-      path: "/signup",
-      element: (
-        <PublicRoute>
-          <Signup />
-        </PublicRoute>
-      ),
-    },
-    {
-      path: "/dailynews",
-      element: (
-        <ProtectedRoute>
-          <DailyNews />
-        </ProtectedRoute>
-      ),
-    },
-    {
-      path: "/allnews",
-      element: (
-        <ProtectedRoute>
-          <AllNews />
-        </ProtectedRoute>
-      ),
-    },
-    {
-      path: "/news/:postId",
-      element: (
-        <ProtectedRoute>
-          <PostDetails />
-        </ProtectedRoute>
-      ),
-    },
-  ]);
-
   return (
     <Provider store={store}>
-      <RouterProvider router={router} />
+      <MainNavigator />
     </Provider>
   );
 }
